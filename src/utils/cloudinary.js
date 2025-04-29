69e05e384a41ac296ea60985e845d295a8e67f85
Rishabh Singh <rishabh4701@gmail.com>
2025-04-29 19:09:54 +0530

    File Uploading

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { log } from 'console';
+import fs from "fs"
+
+
+// Configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
+});
+
+const uploadFile = async (localPath) => {
+    try {
+        if (!localPath) {
+            return null
+        }
+        const response = await cloudinary.uploader.upload(localPath, {
+            resource_type: "auto"
+        })
+        console.log("File has been uploaded", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localPath)
+        return null
+    }
+}
+
+export {uploadFile}
+
