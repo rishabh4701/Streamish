df19516b72d45526a7231a04d34a02b80681ec4c
Rishabh Singh <rishabh4701@gmail.com>
2025-07-10 01:29:29 +0530

    writing routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, registerUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser, registerUser, logoutUser, refreshAccessToken, changePassword, currentUser, updateDetails, updateAvatar, updateCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -20,8 +20,24 @@ router.route("/register").post(
 
 router.route("/login").post(loginUser)
 
+//secured routes
+
 router.route("/logout").post(verifyJWT, logoutUser)  // because of next()
 
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT, changePassword)
+
+router.route("/current-user").get(verifyJWT, currentUser)
+
+router.route("/update-details").patch(verifyJWT, updateDetails) // patch - to not update all details, do not use post
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar) // two middlewares jwt and multer(upload)
+
+router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateCoverImage)
+
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile)  // use : for params 
+
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export {router}
